c73ad29467652fffc44d683d71007b326c9eda97
BlacktonRT <blacktonrt@outlook.com>
2023-03-22 20:54:00 -0600

    Inspector
    Agregué ScripInspector y los scripts sin pulir de PlayerAttack y PlayerMovement

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+	//Objeto que el personaje puede equipar y usar para atacar
+	public GameObject weapon;
+
+	//Fuerza del ataque
+	public float attackForce = 10f;
+
+	//Duración del tiempo de espera después de un ataque
+	public float attackCooldown = 1f;
+
+	//Referencia al componente Rigidbody2D del personaje
+	private Rigidbody2D rb;
+
+	//Variable que indica si el personaje puede atacar o no
+	private bool canAttack = true;
+
+	//Referencia al componente Animator del personaje
+	private Animator anim;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+	}
+
+	void Update()
+	{
+		//Equipar o desequipar el objeto de ataque
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			EquipWeapon();
+		}
+		else if (Input.GetKeyDown(KeyCode.Q))
+		{
+			UnequipWeapon();
+		}
+
+		//Atacar si se presiona el botón de ataque y el personaje puede atacar
+		if (Input.GetKeyDown(KeyCode.F) && canAttack)
+		{
+			Attack();
+		}
+	}
+
+	//Equipar el objeto de ataque
+	void EquipWeapon()
+	{
+		weapon.SetActive(true);
+	}
+
+	//Desequipar el objeto de ataque
+	void UnequipWeapon()
+	{
+		weapon.SetActive(false);
+	}
+
+	//Atacar
+	void Attack()
+	{
+		//Comprobar si el objeto de ataque está equipado
+		if (weapon.activeSelf)
+		{
+			//Obtener la dirección del ataque
+			Vector2 attackDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+
+			//Aplicar la fuerza del ataque al objeto de ataque
+			Rigidbody2D weaponRB = weapon.GetComponent<Rigidbody2D>();
+			weaponRB.velocity = attackDirection * attackForce;
+
+			//Desactivar el objeto de ataque después de un tiempo corto para simular que se rompe
+			StartCoroutine(DisableWeapon());
+
+			//Iniciar la animación de ataque
+			anim.SetTrigger("attack");
+
+			//Establecer el tiempo de espera después del ataque
+			canAttack = false;
+			StartCoroutine(AttackCooldown());
+		}
+	}
+
+	//Desactivar el objeto de ataque después de un tiempo corto para simular que se rompe
+	IEnumerator DisableWeapon()
+	{
+		yield return new WaitForSeconds(0.5f);
+		weapon.SetActive(false);
+	}
+
+	//Establecer el tiempo de espera después del ataque
+	IEnumerator AttackCooldown()
+	{
+		yield return new WaitForSeconds(attackCooldown);
+		canAttack = true;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	//Velocidad horizontal del personaje
+	public float speed = 5f;
+
+	//Velocidad de salto del personaje
+	public float jumpSpeed = 10f;
+
+	//Fuerza de gravedad que afecta al personaje
+	public float gravity = 2f;
+
+	//Referencia al componente Rigidbody2D del personaje
+	private Rigidbody2D rb;
+
+	//Variable que indica si el personaje está en contacto con el suelo o no
+	private bool isGrounded = false;
+
+	//Referencia al transform del objeto que indica si el personaje está en contacto con el suelo
+	public Transform groundCheck;
+
+	//Radio del círculo que se usa para comprobar si el personaje está en contacto con el suelo
+	public float groundCheckRadius = 0.1f;
+
+	//Layer del suelo
+	public LayerMask groundLayer;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		//Comprobar si el personaje está en contacto con el suelo
+		isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+		//Mover horizontalmente el personaje
+		float move = Input.GetAxis("Horizontal");
+		rb.velocity = new Vector2(move * speed, rb.velocity.y);
+
+		//Hacer que el personaje salte si está en contacto con el suelo
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+		{
+			rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+		}
+
+		//Aplicar la fuerza de la gravedad al personaje
+		rb.velocity += new Vector2(0, -gravity * Time.deltaTime);
+	}
+}
