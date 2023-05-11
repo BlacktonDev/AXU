913c1564fbb374f1406a80689918e608ed6e0dfd
rodolfino121 <108913503+rodolfino121@users.noreply.github.com>
2023-05-11 11:24:21 -0600

    enemigo1(no textura, no daño)

---
+++ Assets/Scripts/enemigoPatruya.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemigoPatruya : MonoBehaviour
+{
+	
+	public float speed = 1f; 
+	public float distance = 1f; 
+
+	private Vector3 startPosition; 
+
+	private void Start()
+	{
+		startPosition = transform.position;
+	}
+
+	private void Update()
+	{
+		
+		transform.position = startPosition + new Vector3(Mathf.PingPong(Time.time * speed, distance), 0, 0);
+
+		
+		if (transform.position.x < startPosition.x)
+		{
+			
+			MoveLeftAnim();
+		}
+
+		
+		if (transform.position.x > startPosition.x)
+		{
+			
+			MoveRightAnim();
+		}
+	}
+
+	private void MoveLeftAnim()
+	{
+		
+	}
+
+	private void MoveRightAnim()
+	{
+		
+	}
+		
+}
