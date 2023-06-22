b09d0ff29d67bc581a10aa30297094565b90a52b
Midnightmoon432 <112834510+Midnightmoon432@users.noreply.github.com>
2023-06-22 15:49:36 -0600

    Nuevos fondos / peces

---
+++ Assets/Scripts/Peces.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Peces : MonoBehaviour
+{
+	public GameObject prefabparticula;
+	public float tiempo = 0.5f;
+	public float tiempodestruir = 0f;
+	public float velocidad = 1f;
+	private float tiempoactual = 0f;
+	Camera camara;
+	
+    // Start is called before the first frame update
+	private void Start()
+	{
+		camara = Camera.main;
+	}
+    
+	private void Update()
+	{
+		tiempoactual += Time.deltaTime;
+		if (tiempoactual >= tiempo)
+		{
+			GenerarParticula();
+			tiempoactual = 0f;
+		}
+	}
+
+	void GenerarParticula()
+	{
+		Vector3 PosicionGeneracion = CalcularPosicion();
+		PosicionGeneracion.z = 0f;
+		GameObject particula = Instantiate(prefabparticula,PosicionGeneracion,Quaternion.identity);
+		Rigidbody2D rgb2d = particula.GetComponent<Rigidbody2D>();
+		rgb2d.velocity = Vector2.left * velocidad;
+		
+		Destroy(particula, tiempodestruir);
+    }
+    
+	private Vector3 CalcularPosicion()
+	{
+		float alto = camara.orthographicSize * 2f;
+		float ancho = alto * camara.aspect;
+		
+		Vector3 PosicionGeneracion = camara.transform.position;
+		PosicionGeneracion.y += Random.Range(-alto/2f,alto / 2f);
+		PosicionGeneracion.x = camara.transform.position.x + ancho / 2f;
+		
+		return PosicionGeneracion;
+	}
+}
--- Assets/Scripts/Seguimiento.cs
+++ Assets/Scripts/Seguimiento.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class Seguimiento : MonoBehaviour
 {
 	[SerializeField] private GameObject fondo;
+	[SerializeField] private GameObject fondo1;
+	[SerializeField] private GameObject fondo2;
+	[SerializeField] private GameObject fondo3;
+	[SerializeField] private GameObject fondo4;
+	[SerializeField] private GameObject fondo5;
+	[SerializeField] private GameObject fondo6;
+	[SerializeField] private GameObject fondo7;
 	[SerializeField] private float velocidad;
 	
-	private Renderer fondor;
+	private Renderer fondor,fondo1r, fondo2r,fondo3r,fondo4r,fondo5r,fondo6r,fondo7r;
 	private float incam,difcam;
 	public Vector2 cammin,cammax;
 	public GameObject seguir;
@@ -15,6 +22,13 @@ public class Seguimiento : MonoBehaviour
     void Start()
     {
 	    fondor = fondo.GetComponent<Renderer>();
+	    fondo1r = fondo1.GetComponent<Renderer>();
+	    fondo2r = fondo2.GetComponent<Renderer>();
+	    fondo3r = fondo3.GetComponent<Renderer>();
+	    fondo4r = fondo4.GetComponent<Renderer>();
+	    fondo5r = fondo5.GetComponent<Renderer>();
+	    fondo6r = fondo6.GetComponent<Renderer>();
+	    fondo7r = fondo7.GetComponent<Renderer>();
 	    incam = transform.position.x;
     }
 
@@ -29,10 +43,31 @@ public class Seguimiento : MonoBehaviour
 		//Velocidad de seguimiento de cada fondo
 		difcam = incam - transform.position.x;
 		Vector2 offfon =  new Vector2(difcam * velocidad * -1, 0.0f);
+		Vector2 offfon1 = new Vector2(difcam * velocidad * -2, 0.0f);
+		Vector2 offfon2 = new Vector2(difcam * (velocidad * 1f) * -1, 0.0f);
+		Vector2 offfon3 = new Vector2(difcam * (velocidad * 0.8f) * -1, 0.0f);
+		Vector2 offfon4 = new Vector2(difcam * (velocidad * 0.4f) * -1, 0.0f);
+		Vector2 offfon5 = new Vector2(difcam * (velocidad * 0.4f) * -1, 0.0f);
+		Vector2 offfon6 = new Vector2(difcam * (velocidad * 0.2f) * -1, 0.0f);
+		Vector2 offfon7 = new Vector2(difcam * (velocidad * 0.1f) * -1, 0.0f);
 		
 		fondor.material.mainTextureOffset = offfon;
+		fondo1r.material.mainTextureOffset = offfon1;
+		fondo2r.material.mainTextureOffset = offfon2;
+		fondo3r.material.mainTextureOffset = offfon3;
+		fondo4r.material.mainTextureOffset = offfon4;
+		fondo5r.material.mainTextureOffset = offfon5;
+		fondo6r.material.mainTextureOffset = offfon6;
+		fondo7r.material.mainTextureOffset = offfon7;
 		
 		fondor.transform.position = new Vector3(transform.position.x,fondor.transform.position.y,fondor.transform.position.z);
-        
+		fondo1r.transform.position = new Vector3(transform.position.x,fondo1r.transform.position.y,fondo1r.transform.position.z);
+		fondo2r.transform.position = new Vector3(transform.position.x,fondo2r.transform.position.y,fondo2r.transform.position.z);
+		fondo3r.transform.position = new Vector3(transform.position.x,fondo3r.transform.position.y,fondo3r.transform.position.z);
+		fondo4r.transform.position = new Vector3(transform.position.x,fondo4r.transform.position.y,fondo4r.transform.position.z);
+		fondo5r.transform.position = new Vector3(transform.position.x,fondo5r.transform.position.y,fondo5r.transform.position.z);
+		fondo6r.transform.position = new Vector3(transform.position.x,fondo6r.transform.position.y,fondo6r.transform.position.z);
+		fondo7r.transform.position = new Vector3(transform.position.x,fondo7r.transform.position.y,fondo7r.transform.position.z);
+		
     }
 }
