a6d60c60dac13d645b2ae086607edb3d80bf7fd2
rodolfino121 <108913503+rodolfino121@users.noreply.github.com>
2023-06-23 12:26:57 -0600

    plataforma, rana, coliders.

--- Assets/Scripts/Rana.cs
+++ Assets/Scripts/Rana.cs
@@ -4,42 +4,73 @@ using UnityEngine;
 
 public class Rana : MonoBehaviour
 {
-	public float jumpForce = 7f; // Fuerza de salto
+	public float jumpForce = 5f; // Fuerza de salto
 	public float movementSpeed = 3f; // Velocidad de movimiento horizontal
+	public float detectionRange = 10f; // Rango de detección para seguir al jugador
 	public LayerMask groundLayer; // Layer que representa el suelo
+	public string playerTag = "Player"; // Tag del objeto jugador
 
 	private Rigidbody2D rb;
+	private GameObject player;
 	private bool isJumping = false;
 	private bool isOnGround = false;
-	private GameObject jugador;
+	private bool isFalling = false;
 
-	private void Awake()
+	private void Start()
 	{
 		rb = GetComponent<Rigidbody2D>();
-		BuscarJugador();
+		player = GameObject.FindGameObjectWithTag(playerTag);
 	}
 
 	private void Update()
 	{
-		if (jugador != null && isJumping)
+		if (player != null)
 		{
-			// Movimiento horizontal hacia la dirección del jugador
-			Vector2 direction = (jugador.transform.position - transform.position).normalized;
-			rb.velocity = new Vector2(direction.x * movementSpeed, rb.velocity.y);
+			float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+
+			// Comprobar si la distancia al jugador es menor que el rango de detección
+			if (distanceToPlayer < detectionRange)
+			{
+				// Calcular dirección hacia el jugador
+				Vector2 direction = player.transform.position - transform.position;
+				direction.Normalize();
+
+				// Verificar si hay obstáculos a la izquierda o derecha
+				RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, Vector2.left, 1f, groundLayer);
+				RaycastHit2D hitRight = Physics2D.Raycast(transform.position, Vector2.right, 1f, groundLayer);
+
+				// Moverse hacia el jugador en el eje X solo si no hay obstáculos en los lados
+				if (!hitLeft && !hitRight)
+				{
+					rb.velocity = new Vector2(direction.x * movementSpeed, rb.velocity.y);
+				}
+				else
+				{
+					rb.velocity = new Vector2(0f, rb.velocity.y);
+				}
+
+				// Saltar si se encuentra en el suelo y no está cayendo
+				if (isOnGround && !isFalling)
+				{
+					isJumping = true;
+					rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+				}
+			}
+			else
+			{
+				// Detener el movimiento si la distancia al jugador es mayor que el rango de detección
+				rb.velocity = new Vector2(0f, rb.velocity.y);
+			}
+		}
+
+		// Verificar si el objeto está cayendo
+		if (rb.velocity.y < 0f)
+		{
+			isFalling = true;
 		}
 		else
 		{
-			rb.velocity = Vector2.zero; // Detener el movimiento si el jugador no está asignado o no está saltando
-		}
-	}
-
-	private void FixedUpdate()
-	{
-		if (isOnGround && isJumping)
-		{
-			// Salto
-			rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-			isOnGround = false; // Restablecer el estado de estar en el suelo
+			isFalling = false;
 		}
 	}
 
@@ -52,13 +83,12 @@ public class Rana : MonoBehaviour
 		}
 	}
 
-	private void BuscarJugador()
+	private void OnCollisionExit2D(Collision2D collision)
 	{
-		GameObject jugadorObj = GameObject.FindGameObjectWithTag("Player");
-		if (jugadorObj != null)
+		// Detección de salida de colisión con el suelo
+		if (((1 << collision.gameObject.layer) & groundLayer) != 0)
 		{
-			jugador = jugadorObj;
-			isJumping = true;
+			isOnGround = false;
 		}
 	}
 }
--- Assets/Scripts/enemigoPatrulla.cs
+++ Assets/Scripts/enemigoPatrulla.cs
@@ -9,8 +9,14 @@ public class enemigoPatrulla : MonoBehaviour
 	public float distance = 1f;
 	public int saludMaxima = 100;
 	private int saludActual;
+	private Collider2D[] objectsOnTop;
+	private string[] validTags = { "Player", "Enemy" };
 
 	private Vector3 startPosition; 
+	
+	
+	private bool isMovingLeft = true;
+	private Collider2D objectOnTop;
 
 	private void Start()
 	{
@@ -21,21 +27,55 @@ public class enemigoPatrulla : MonoBehaviour
 	private void Update()
 	{
 		
-		transform.position = startPosition + new Vector3(Mathf.PingPong(Time.time * speed, distance), 0, 0);
-
-		
-		if (transform.position.x < startPosition.x)
+		if (isMovingLeft)
 		{
-			
-			MoveLeftAnim();
+			transform.Translate(Vector3.left * speed * Time.deltaTime);
+		}
+		else
+		{
+			transform.Translate(Vector3.right * speed * Time.deltaTime);
 		}
 
-		
-		if (transform.position.x > startPosition.x)
+		// Cambio de dirección cuando alcanza los límites
+		if (transform.position.x <= startPosition.x - 2f)
 		{
-			
-			MoveRightAnim();
+			isMovingLeft = false;
 		}
+		else if (transform.position.x >= startPosition.x + 2f)
+		{
+			isMovingLeft = true;
+		}
+
+		// Movimiento del objeto sobre el objeto principal
+		if (objectOnTop != null)
+		{
+			objectOnTop.transform.position = transform.position + Vector3.up;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		// Detección de colisión con otro objeto
+		if (collision.CompareTag("Player") || collision.CompareTag("Enemy"))
+		{
+			collision.transform.parent = transform;
+			UpdateObjectsOnTop();
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		// Detección de salida de colisión con otro objeto
+		if (collision.CompareTag("Player") || collision.CompareTag("Enemy"))
+		{
+			collision.transform.parent = null;
+			UpdateObjectsOnTop();
+		}
+	}
+
+	private void UpdateObjectsOnTop()
+	{
+		objectsOnTop = GetComponentsInChildren<Collider2D>();
 	}
 
 	private void MoveLeftAnim()
