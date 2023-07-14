0c15d6bb008a8e9fddf77bf13f2ac749b3493082
Midnightmoon432 <112834510+Midnightmoon432@users.noreply.github.com>
2023-07-13 18:41:10 -0600

    mapa3/musica/narrador

---
+++ Assets/Scripts/Dialogos.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogos : MonoBehaviour
+{
+	[SerializeField] GameObject txtdialogos;
+	[SerializeField] GameObject dialogos;
+	private Rigidbody2D rb;
+	private bool isInsideCollider = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+	    rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    if (isInsideCollider)
+	    {
+		    txtdialogos.SetActive(true);
+		    dialogos.SetActive(true);
+	    }
+	    else
+	    {
+		    txtdialogos.SetActive(false);
+		    dialogos.SetActive(false);
+	    }
+    }
+    
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Player"))
+		{
+			isInsideCollider = true;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Player"))
+		{
+			isInsideCollider = false;
+		}
+	}
+}
---
+++ Assets/Scripts/Plataformadestruible.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plataformadestruible : MonoBehaviour
+{
+	private bool playerDetected = false;
+	private bool isPlatformActive = true;
+	public float deactivationTime = 5f;
+	public float reactivationTime = 5f;
+	private float timer = 0f;
+	private Material originalMaterial;
+	public Material deactivationMaterial;
+	private Renderer platformRenderer;
+	
+	void Start()
+	{
+		// Obtener el componente Renderer
+		platformRenderer = GetComponent<Renderer>();
+
+		// Almacenar el material original
+		originalMaterial = platformRenderer.material;
+	}
+    
+	void Update()
+	{
+		if (playerDetected && isPlatformActive)
+		{
+			timer += Time.deltaTime;
+
+			if (timer >= deactivationTime)
+			{
+				isPlatformActive = false;
+				timer = 0f;
+				// Desactivar la plataforma (por ejemplo, deshabilitar el Rigidbody2D)
+				GetComponent<Rigidbody2D>().simulated = false;
+			}
+		}
+		else if (!isPlatformActive)
+		{
+			timer += Time.deltaTime;
+
+			if (timer >= reactivationTime)
+			{
+				isPlatformActive = true;
+				timer = 0f;
+				// Volver a activar la plataforma (por ejemplo, habilitar el Rigidbody2D)
+				GetComponent<Rigidbody2D>().simulated = true;
+			}
+		}
+		
+		if (isPlatformActive)
+		{
+			platformRenderer.material = originalMaterial;
+			// Resto del código...
+		}
+		else
+		{
+			platformRenderer.material = deactivationMaterial;
+			// Resto del código...
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			playerDetected = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			playerDetected = false;
+		}
+	}
+}
